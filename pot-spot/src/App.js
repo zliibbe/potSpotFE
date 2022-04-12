322267fa82a521d235c3b0c0a0ee86661148a43d
Zach Liibbe <zliibbe@gmail.com>
2022-04-12 10:25:45 -0600

    Add class-based App component

--- pot-spot/src/App.js
+++ pot-spot/src/App.js
@@ -1,11 +1,48 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-   <main className=''>
-
-   </main>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = { potholes: [ 
+        {
+        latitude: '39.74379494415912',
+        longitude: '-104.95005172109876',
+        description: 'Decent size',
+        pictures: [
+          'https://www.attorneystevelee.com/wp-content/uploads/pothole-road1.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Large_pot_hole_on_2nd_Avenue_in_New_York_City.JPG/1920px-Large_pot_hole_on_2nd_Avenue_in_New_York_City.JPG'
+        ]
+        }, 
+        {
+          latitude: '39.74018534594094',
+          longitude: '-104.95724927698312',
+          description: `its a pothole`,
+          pictures: [
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Asphalt_deterioration.jpg/1024px-Asphalt_deterioration.jpg',
+          ]
+        }, 
+        {
+        latitude: '39.77998918688553',
+        longitude: '-104.97897473238706',
+        description: 'Extra Smelly',
+        pictures: [
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Newport_Whitepit_Lane_pot_hole.JPG/1920px-Newport_Whitepit_Lane_pot_hole.JPG',
+          'https://www.thebalance.com/thmb/VlnrT3pRKvtegoumE0fXWmA4pWI=/2121x1193/smart/filters:no_upscale()/pothole-174662203-5a7dc84aae9ab80036c6ad36.jpg'
+        ]
+      }] 
+    }
+  }
+  
+  render() {
+    return (
+      <main className='App'>
+        <h1 className='title'>Pot Spot</h1>
+        <Form />
+   
+      </main>
+     )
+  } ;
 }
 
 export default App;
