7c30a24ce0c6fb6f46b514a23640a6d7fdd17c8a
Zach Liibbe <zliibbe@gmail.com>
2022-04-12 10:40:36 -0600

    Add Form component

---
+++ pot-spot/src/components/Form/Form.js
@@ -0,0 +1,56 @@
+import React from "react"
+import './Form.css'
+
+class Form extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            latitude: '',
+            longitude: '',
+            description: '',
+            pictures: []
+        }
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    render() {
+        return (
+            <form className='pothole-form'>
+                <input 
+                    type="text"
+                    name="latitude"
+                    placeholder="Enter Latitude"
+                    value={this.state.latitude}
+                    onChange={event => handleChange(event)}    
+                ></input>
+
+                <input 
+                    type="text"
+                    name="longitude"
+                    placeholder="Enter Longitude"
+                    value={this.state.longitude}
+                    onChange={event => handleChange(event)}    
+                ></input>
+
+                <input 
+                    type="text"
+                    name="description"
+                    placeholder="Describe pothole"
+                    value={this.state.description}
+                    onChange={event => handleChange(event)}    
+                ></input>
+
+                <input 
+                    type="text"
+                    name="pictures"
+                    placeholder="Paste URL of pothole photo"
+                    value={this.state.pictures}
+                    onChange={event => handleChange(event)}    
+                ></input>
+            </form>
+        )
+    }
+}
