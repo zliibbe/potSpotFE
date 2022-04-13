e4faa6a50273f9c72028cb081847ae9b99db369f
Ronald Head <ronaldlhead@gmail.com>
2022-04-13 09:53:11 -0600

    Set up display for current pothole display

--- pot-spot/src/components/PotholeDetail/PotholeDetail.js
+++ pot-spot/src/components/PotholeDetail/PotholeDetail.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-const PotholeDetail = () => {
-    // const singlePothole = potholes.find(pothole => {
+const PotholeDetail = ({currentPothole, collectPotholePhotos}) => {
+    
 
-    // })
+    return (
+        <section className='pothole-detail'>
+        <div className="location-detail">
+            <h2>Location</h2>
+            <p className="latitude-detail">{currentPothole.latitude}</p>
+            <p className="longitude-detail">{currentPothole.longitude}</p>
+        </div>
+        <div className="description-detail">{currentPothole.description}</div>
+        <div className="pictures-detail" >{collectPotholePhotos()}</div>
+    </section>
+    )
+
 
 }
 
