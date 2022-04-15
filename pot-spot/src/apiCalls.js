98221e2d13c9166a3551ea326a58a3b19769b256
Jake Dunafon <jlad795@gmail.com>
2022-04-15 14:42:13 -0700

    Add pictures fetch

--- pot-spot/src/apiCalls.js
+++ pot-spot/src/apiCalls.js
@@ -11,4 +11,17 @@ const fetchPotholes = (id) => {
     return fetchCall
 }
 
-export { fetchPotholes }
+const fetchPictures = () => {
+    const fetchCall = fetch('https://pot-spot.herokuapp.com/api/v1/pictures')
+     .then(response => {
+         if (response.status === 404) {
+             throw new Error("404: Not Found")
+         } else if (response.status === 500) {
+             throw new Error("500: Server is having issues")
+         }
+         return response.json()
+     })
+     return fetchCall
+ }
+
+export { fetchPotholes, fetchPictures }
