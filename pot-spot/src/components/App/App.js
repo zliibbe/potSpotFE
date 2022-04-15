6dfd7c7126ce65c8f13d442e5d6af155e503e26b
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-15 12:06:35 -0600

    Remove google api imports

--- pot-spot/src/components/App/App.js
+++ pot-spot/src/components/App/App.js
@@ -5,7 +5,6 @@ import Form from '../Form/Form'
 import PotholeDetail from '../PotholeDetail/PotholeDetail'
 import { fetchPotholes } from '../../apiCalls';
 import Map from '../Map/Map';
-import { GoogleMap, LoadScript } from "@react-google-maps/api"
 
 // AIzaSyDjRoZ4cq5mC6f_bIYDVFwcZDnQluNKI6Q
 // AIzaSyCNdw_FpGRuXLBOf6iS_K7qYCg3YrsRti8
@@ -14,37 +13,6 @@ class App extends React.Component {
   constructor () {
     super();
     this.state = { potholes: [],
-
-      // [
-      //   {
-      //   id: 1,
-      //   latitude: '39.74379494415912',
-      //   longitude: '-104.95005172109876',
-      //   description: 'Decent size',
-      //   pictures: [
-      //     'https://www.attorneystevelee.com/wp-content/uploads/pothole-road1.jpg',
-      //     'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Large_pot_hole_on_2nd_Avenue_in_New_York_City.JPG/1920px-Large_pot_hole_on_2nd_Avenue_in_New_York_City.JPG'
-      //   ]
-      //   },
-      //   {
-      //     id: 2,
-      //     latitude: '39.74018534594094',
-      //     longitude: '-104.95724927698312',
-      //     description: `its a pothole`,
-      //     pictures: [
-      //       'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Asphalt_deterioration.jpg/1024px-Asphalt_deterioration.jpg',
-      //     ]
-      //   },
-      //   {
-      //     id: 3,
-      //     latitude: '39.77998918688553',
-      //     longitude: '-104.97897473238706',
-      //     description: 'Extra Smelly',
-      //     pictures: [
-      //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Newport_Whitepit_Lane_pot_hole.JPG/1920px-Newport_Whitepit_Lane_pot_hole.JPG',
-      //       'https://www.thebalance.com/thmb/VlnrT3pRKvtegoumE0fXWmA4pWI=/2121x1193/smart/filters:no_upscale()/pothole-174662203-5a7dc84aae9ab80036c6ad36.jpg'
-      //     ]
-        // }],
         currentPothole: ''
     }
   }
@@ -87,14 +55,14 @@ class App extends React.Component {
     } else {
       display=
       <main className='App'>
+
         <h1 className='title'>Pot Spot</h1>
         <h2>Denver, CO</h2>
+
         <div className='form-map-container'>
           <Form addPothole={this.addPothole}/>
+
           <div className='pothole-form map-placeholder'>
-            <LoadScript
-              googleMapsApiKey='AIzaSyCNdw_FpGRuXLBOf6iS_K7qYCg3YrsRti8'
-              />
             <Map potholes={this.state.potholes} />
           </div>
         </div>
