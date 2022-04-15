1f9ba392c64eca050058abbb4e40cc53bffebd6a
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-15 12:07:09 -0600

    Convert map to use react-google-maps/api

--- pot-spot/src/components/Map/Map.js
+++ pot-spot/src/components/Map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {GoogleMap, Marker, InfoWindow} from "@react-google-maps/api";
+import {GoogleMap, Marker, InfoWindow, LoadScript} from "@react-google-maps/api";
 
 class Map extends Component {
   constructor(props) {
@@ -17,17 +17,19 @@ class Map extends Component {
   }
   render() {
     return (
-        <GoogleMap
-            zoom={10}
-            center={{lat:39.742043, lng:-104.991531 }}
-            containerStyle={{
-              width: '100%',
-              height: '100%'
-            }}
-        >
 
+      <LoadScript
+        googleMapsApiKey="AIzaSyCNdw_FpGRuXLBOf6iS_K7qYCg3YrsRti8">
+        <GoogleMap
+          zoom={12}
+          center={{lat:39.742043, lng:-104.991531 }}
+          mapContainerStyle={{
+            width: '100%',
+            height: '100%'
+          }}>
         {this.props.potholes.map(pothole => {
-          return (<Marker
+          return (
+            <Marker
               key={pothole.id}
               position={{
                 lat: parseFloat(pothole.latitude),
@@ -36,14 +38,42 @@ class Map extends Component {
               onClick={(event) => {
                 this.displayInfoWindow(pothole)
               }}
+
+
             />)
         })}
 
+        {this.state.currentPothole && (
+          <InfoWindow
+            onCloseClick={() => {
+              this.closeWindow();
+            }}
+            position={{
+              lat:parseFloat(this.state.currentPothole.latitude),
+              lng: parseFloat(this.state.currentPothole.longitude)
+            }}
+            >
+              <div>
+                <p>{this.state.currentPothole.description}</p>
+              </div>
+
+          </InfoWindow>
+        )}
 
       </GoogleMap>
+      </LoadScript>
+
     )
   }
   /*
+  <Marker
+  position={{
+  lat: parseFloat(this.props.potholes[0].latitude),
+  lng: parseFloat(this.props.potholes[0].longitude)
+  }}
+  ></Marker>
+
+
   {this.state.currentPothole && (
     <InfoWindow
       onClick={this.closeWindow}
@@ -58,6 +88,20 @@ class Map extends Component {
     </InfoWindow>
 
   )}
+
+
+  {this.props.potholes.map(pothole => {
+  return (<Marker
+  key={pothole.id}
+  position={{
+  lat: parseFloat(pothole.latitude),
+  lng: parseFloat(pothole.longitude)
+  }}
+  onClick={(event) => {
+  this.displayInfoWindow(pothole)
+  }}
+  />)
+  })}
   */
 }
 
