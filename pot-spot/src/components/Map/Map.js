693036b3851e03009b726b535718552633480876
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-14 18:07:43 -0600

    Add image to assets

--- pot-spot/src/components/Map/Map.js
+++ pot-spot/src/components/Map/Map.js
@@ -1,25 +1,60 @@
-import React from "react";
-import {GoogleMap, Marker} from "react-google-maps";
+import React, { Component } from "react";
+import {GoogleMap, Marker, InfoWindow} from "react-google-maps";
 
-function Map({potholes}) {
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      currentPothole: null
+    }
+  }
 
+  displayInfoWindow =(pothole) => {
+    this.setState({currentPothole: pothole})
+  }
+  closeWindow = () => {
+    this.setState({currentPothole: null})
+  }
+  render() {
     return (
         <GoogleMap
             defaultZoom={10}
             defaultCenter={{lat:39.742043, lng:-104.991531 }}
         >
 
-        {potholes.map(pothole => {
+        {this.props.potholes.map(pothole => {
           return (<Marker
               key={pothole.id}
               position={{
                 lat: parseFloat(pothole.latitude),
                 lng: parseFloat(pothole.longitude)
               }}
+              onClick={(event) => {
+                this.displayInfoWindow(pothole)
+              }}
             />)
         })}
+
+
       </GoogleMap>
     )
+  }
+  /*
+  {this.state.currentPothole && (
+    <InfoWindow
+      onClick={this.closeWindow}
+      position={{
+        lat:parseFloat(this.state.currentPothole.latitude),
+        lng: parseFloat(this.state.currentPothole.longitude)
+      }}>(
+        <div>
+          <p>Hello world</p>
+        </div>
+      )
+    </InfoWindow>
+
+  )}
+  */
 }
 
 export default Map
