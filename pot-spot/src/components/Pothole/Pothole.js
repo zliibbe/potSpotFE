5d06516606648f884a825f12cb93de73fe88e442
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-16 14:00:51 -0600

    Convert to only load when provided information

--- pot-spot/src/components/Pothole/Pothole.js
+++ pot-spot/src/components/Pothole/Pothole.js
@@ -1,22 +1,28 @@
 import React from "react"
 import './Pothole.css'
 
-const Pothole = ({latitude, longitude, description, pictures, id, findPothole}) => {
+const Pothole = (props) => {
     // const photoElements = pictures.map(picture => {
     //     return <a href={ picture }><img alt={picture} src={picture}/> </a>
     // })
     // const potholePreviewPhoto = <img src={pictures[0]} className='photo-preview'/>
+    let potholeDisplay = ''
+    if(props.pothole) {
+      const {latitude, longitude, description, pictures, id, findPothole} = props.pothole;
+      potholeDisplay = <section className='pothole' onClick={() => findPothole(id)}>
+        <div className="location">
+          <h3>Location</h3>
+          <p className="latitude">{latitude}</p>
+          <p className="longitude">{longitude}</p>
+        </div>
+        <div className="description">{description}</div>
+      </section>
+    }
 
     return (
-        <section className='pothole' onClick={() => findPothole(id)}>
-            <div className="location">
-                <h3>Location</h3>
-                <p className="latitude">{latitude}</p>
-                <p className="longitude">{longitude}</p>
-            </div>
-            <div className="description">{description}</div>
-            
-        </section>
+      <React.Fragment>
+        {potholeDisplay}
+      </React.Fragment>
     )
 }
 
