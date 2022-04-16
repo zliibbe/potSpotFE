c854d6647fb1c9085e00456485028da3d187a4e8
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-04-16 13:36:31 -0600

    Create home button with Link

--- pot-spot/src/components/Header/Header.js
+++ pot-spot/src/components/Header/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
-
-const Header = () => {
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Header = (props) => {
+  let button = props.home ? <Link to='/home'><button className='home-button'>Home</button></Link> : '' ;
     return (
         <header>
           <h1 className='title'>Pot Spot</h1>
           <h2 className='title'>Denver, CO</h2>
+          {button}
         </header>
     )
 }
