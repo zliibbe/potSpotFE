eb03e8253aa0eb77a1af6e49e24727514ee73269
Zach Liibbe <zliibbe@gmail.com>
2022-04-17 14:19:28 -0600

    Add deletePothole method to apiCalls

--- pot-spot/src/components/Pothole/Pothole.js
+++ pot-spot/src/components/Pothole/Pothole.js
@@ -1,35 +1,44 @@
-import React from "react"
-import './Pothole.css'
+import React from "react";
+import "./Pothole.css";
 
 const Pothole = (props) => {
-  let potholeDisplay = ''
-  let picturesDisplay = ''
+  let potholeDisplay = "";
+  let picturesDisplay = "";
   if (props.pothole && props.potholePictures) {
     const { latitude, longitude, description, id, findPothole } = props.pothole;
-    const pictures = props.potholePictures
-    picturesDisplay = pictures.map(picture => {
-      return <img className="pictures" key={picture.url} alt="Pothole" src={picture.url} />
-    })
-    potholeDisplay = <section className='pothole' onClick={() => findPothole(id)}>
-      <div className="location">
-        <h3>Pothole {id}</h3>
-        <p className="latitude">{latitude}</p>
-        <p className="longitude">{longitude}</p>
-      </div>
-      <div className="description">{description}</div>
-    </section>
+    const pictures = props.potholePictures;
+    picturesDisplay = pictures.map((picture) => {
+      return (
+        <img
+          className="pictures"
+          key={picture.url}
+          alt="Pothole"
+          src={picture.url}
+        />
+      );
+    });
+    potholeDisplay = (
+      <section className="pothole" onClick={() => findPothole(id)}>
+        <div className="location">
+          <h3>Pothole {id}</h3>
+          <p className="latitude">{latitude}</p>
+          <p className="longitude">{longitude}</p>
+        </div>
+        <div className="description">{description}</div>
+      </section>
+    );
   }
+
+
 
   return (
     <React.Fragment>
       {potholeDisplay}
-      <section className="pictures-section">
-        {picturesDisplay}
-      </section>
+      <section className="pictures-section">{picturesDisplay}</section>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Pothole
+export default Pothole;
 
 // {/* <div className="pictures" >{potholePreviewPhoto}</div> */}
