d74a65740da83da6d8a541da0041883a41aa019d
Jake Dunafon <jlad795@gmail.com>
2022-04-17 10:24:34 -0700

    Add css to pictures

--- pot-spot/src/components/Pothole/Pothole.js
+++ pot-spot/src/components/Pothole/Pothole.js
@@ -2,32 +2,32 @@ import React from "react"
 import './Pothole.css'
 
 const Pothole = (props) => {
-    let potholeDisplay = ''
-    let picturesDisplay = ''
-    if(props.pothole && props.potholePictures) {
-      const {latitude, longitude, description, id, findPothole} = props.pothole;
-      const pictures = props.potholePictures
-      console.log(pictures)
-      console.log(props)
-      picturesDisplay = pictures.map(picture => {
-        return <img key={picture.pothole_id} alt="Pothole" src={picture.url}/>
-      })
-      potholeDisplay = <section className='pothole' onClick={() => findPothole(id)}>
-        <div className="location">
-          <h3>Location</h3>
-          <p className="latitude">{latitude}</p>
-          <p className="longitude">{longitude}</p>
-        </div>
-        <div className="description">{description}</div>
-      </section>
-    }
+  let potholeDisplay = ''
+  let picturesDisplay = ''
+  if (props.pothole && props.potholePictures) {
+    const { latitude, longitude, description, id, findPothole } = props.pothole;
+    const pictures = props.potholePictures
+    picturesDisplay = pictures.map(picture => {
+      return <img className="pictures" key={picture.url} alt="Pothole" src={picture.url} />
+    })
+    potholeDisplay = <section className='pothole' onClick={() => findPothole(id)}>
+      <div className="location">
+        <h3>Pothole {id}</h3>
+        <p className="latitude">{latitude}</p>
+        <p className="longitude">{longitude}</p>
+      </div>
+      <div className="description">{description}</div>
+    </section>
+  }
 
-    return (
-      <React.Fragment>
-        {potholeDisplay}
+  return (
+    <React.Fragment>
+      {potholeDisplay}
+      <section className="pictures-section">
         {picturesDisplay}
-      </React.Fragment>
-    )
+      </section>
+    </React.Fragment>
+  )
 }
 
 export default Pothole
