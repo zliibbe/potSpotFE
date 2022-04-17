2f91796162df74792ffb44332f7c1ecc9f821167
Jake Dunafon <jlad795@gmail.com>
2022-04-17 09:27:51 -0700

    Add status board button

--- pot-spot/src/components/Header/Header.js
+++ pot-spot/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = (props) => {
-  let button = props.home ? <Link to='/home'><button className='home-button'>Home</button></Link> : <Link to='/statusboard'></Link> ;
+  let button = props.home ? <Link to='/home'><button className='home-button'>Home</button></Link> : <Link to='/statusboard'><button className='status-button'>Status Board</button></Link> ;
     return (
         <header>
           <h1 className='title'>Pot Spot</h1>
