4511bce83a31af79ed609478e1ad319ca945aaff
Zach Liibbe <zliibbe@gmail.com>
2022-04-17 22:39:10 -0600

    Add class names for cypress

--- pot-spot/src/components/StatusUpdater/StatusUpdater.js
+++ pot-spot/src/components/StatusUpdater/StatusUpdater.js
@@ -4,7 +4,7 @@ const StatusUpdater = (props) => {
 
     return (
         <div>
-            <button onClick={() => props.changeStatus(props.pothole)}> Change Status </button>
+            <button className="change-status" onClick={() => props.changeStatus(props.pothole)}> Change Status </button>
         </div>
     )
 }
