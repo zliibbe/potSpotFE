e1248f8b8c71499af931696f13f38a131850e23e
Zach Liibbe <zliibbe@gmail.com>
2022-04-17 22:37:56 -0600

    Add tests for status board

--- pot-spot/cypress/integration/pot-spot-user-flow.js
+++ pot-spot/cypress/integration/pot-spot-user-flow.js
@@ -1,3 +1,4 @@
+
 describe('User flows for Pot Spot App', () => {
     beforeEach(() => {
       cy.visit('http://localhost:3000')
@@ -23,22 +24,17 @@ describe('User flows for Pot Spot App', () => {
                               ]
               }
           ]
-        }).as('get-potholes')
+        }).as('get-potholes'),
+        
         cy.intercept("GET", "https://maps.googleapis.com/maps/api/mapsjs/gen_204?csp_test=true", {
 
         })
       })
 
-    it('Should confirm that true is equal to true', () => {
-        expect(true).to.equal(true)
-    })
-
     it('should have a form that you can submit new information for a pothole with', () => {
-        cy.get('.pothole-form').contains('Submit New Pothole:')
-        
         cy.get('.pothole-form').then(($form) => {
             cy.contains('Submit New Pothole:')
-            cy.get('input[name="latitude"]').type('39.74379494415912')
+            .get('input[name="latitude"]').type('39.74379494415912')
             .get('input[name="longitude"]').type('-104.95005172109876')
             .get('input[name="description"]').type('Not too bad of a pothole')
             .get('input[name="pictures"]').type('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj44l3nUq20cbLxT_Y7h-eljdoLPwmNiLRjWlV8nU3Hwjjeiml2YH-44OG7NEpzyrb8Gw&usqp=CAU')
@@ -46,6 +42,37 @@ describe('User flows for Pot Spot App', () => {
             .get(".message").contains("Your pothole has been added.")
         })
     })
+
+    it('should have a status board of pothole details that can be accessed', () => {
+        cy.get(".header-button").click().then(($pothole) => {
+            cy.get(".status-pothole").first()
+            .contains("Pothole #6")
+            .contains("Current Status: Pending")
+            .get(".pothole-name").contains()
+        })
+    })
+
+    it('should have a status board that allows for the potholes\' status to be changed', () => {
+        cy.get(".header-button").click().then(($pothole) => {
+            cy.get(".status-pothole").first()
+            .contains("Current Status: Pending")
+            // .contains("")
+            // .get(".pothole-name").contains()
+        })
+    })
     
+    it('should have a status board of pothole details that can be accessed', () => {
+        cy.get(".header-button").click().then(($pothole) => {
+            cy.get(".status-pothole").first()
+            .get(".change-status").click()
+            .get(".status-pothole").third()
+            .contains("Current Status: Being worked on")
+        })
+    })
+     
+    
+    it('should have map that displays existing potholes', () => {
+        // cy.get()
+    })
     
 })
